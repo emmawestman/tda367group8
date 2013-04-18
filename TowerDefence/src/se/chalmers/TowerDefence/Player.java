87c1c3eb171ff51d53d33b4a809245c0bce5f724
Julia Friberg <juliafriberg93@gmail.com>
2013-04-18 13:50:36 +0200

    Added player class

---
+++ TowerDefence/src/se/chalmers/TowerDefence/Player.java
@@ -0,0 +1,30 @@
+package se.chalmers.TowerDefence;
+
+public class Player {
+	
+	private int lives;
+	private int points = 0;
+	private int resources;
+	
+	public Player(int lives, int resources) {
+		this.lives = lives;
+		this.resources = resources;
+	}
+	
+	public void looseLife() {
+		lives--;
+	}
+	
+	public void addPoints(int points) {
+		this.points += points;
+	}
+	
+	public void addResources(int resources) {
+		this.resources += resources;
+	}
+	
+	public void useResources(int cost) {
+		resources -= cost;
+	}
+
+}
