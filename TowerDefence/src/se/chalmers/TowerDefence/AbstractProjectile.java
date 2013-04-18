3821238af305cc15969351e18b7f737366c8ad74
Oskar PJ Dahlberg <oskar.pj.dahlberg@gmail.com>
2013-04-18 16:24:46 +0200

    Projectiles have the same speed all the time

--- TowerDefence/src/se/chalmers/TowerDefence/AbstractProjectile.java
+++ TowerDefence/src/se/chalmers/TowerDefence/AbstractProjectile.java
@@ -7,16 +7,19 @@ import se.chalmers.slickTD.ProjectileView;
 public class AbstractProjectile {
 	private float xPos;
 	private float yPos;
+	private float width;
 	private ProjectileView pV;
 	private AbstractMonster monster;
 	private float speed=5f;
 	private int damage = 5;
 	private boolean exists = true;
+	private double angleX, angleY;
 	public AbstractProjectile(float x, float y, AbstractMonster monster) {
-		this.xPos = x;
-		this.yPos = y;
+		this.xPos = x + 20 + width;
+		this.yPos = y + 20 + width;
 		this.monster = monster;
 		pV=new ProjectileView();
+		width = 5;
 	}
 	
 	public void draw() {
@@ -24,17 +27,27 @@ public class AbstractProjectile {
 	}
 	
 	public void move() {
-		if(xPos<monster.getX()){
-			xPos+=speed;
-		}else if(xPos>monster.getX()){
-			xPos-=speed;
+		if(!monster.isAlive()){
+			exists = false;
 		}
-		if(yPos<monster.getY()){
-			yPos+=speed;
-		}else if(yPos>monster.getY()){
-			yPos-=speed;
-		}
-		if(xPos==monster.getX() && yPos==monster.getY()){
+		double devider = 1/Math.sqrt(Math.pow(monster.getX()-xPos,2) + Math.pow(monster.getY()-yPos, 2));
+		System.out.println("" + devider);
+		this.angleX =  devider * (monster.getX() + 20 - width - xPos);
+		this.angleY =  devider * (monster.getY() + 20 - width - yPos);
+		xPos += angleX * speed;
+		yPos += angleY * speed;
+//		Could be a fun movmentpattern for a sub-projectile, but not how all projectiles move...
+//		if(xPos<monster.getX()+20-width){
+//			xPos+=speed;
+//		}else if(xPos>monster.getX()+20-width){
+//			xPos-=speed;
+//		}
+//		if(yPos<monster.getY()+20-width){
+//			yPos+=speed;
+//		}else if(yPos>monster.getY()+20-width){
+//			yPos-=speed;
+//		}
+		if(xPos>=monster.getX()+10-width && yPos>=monster.getY()+10-width && xPos<=monster.getX()+30-width && yPos<=monster.getY()+30-width){
 			damage();
 			exists = false;
 		}
