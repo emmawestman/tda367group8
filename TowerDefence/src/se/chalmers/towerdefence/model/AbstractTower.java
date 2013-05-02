7bf1c39a77267234acc16df4d7eeff13519bb400
Oskar PJ Dahlberg <oskar.pj.dahlberg@gmail.com>
2013-05-02 15:38:21 +0200

    Towers can now target the monster with lowest HP

--- TowerDefence/src/se/chalmers/towerdefence/model/AbstractTower.java
+++ TowerDefence/src/se/chalmers/towerdefence/model/AbstractTower.java
@@ -3,45 +3,68 @@ package se.chalmers.towerdefence.model;
 import java.util.ArrayList;
 import java.util.List;
 
-import se.chalmers.towerdefence.gui.TowerView;
 
 public abstract class AbstractTower {
 	private float xPos;
 	private float yPos;
 	private int range;
 	private int timer;
+	private final int reloadTime;
 	private ArrayList <AbstractProjectile> projectiles;
 	private int cost;
 	private int damage;
+	private enum Priorities {FIRTS, WEAKEST};
+	private Priorities prioritie;
 
-	public AbstractTower(float x, float y, int range, ArrayList <AbstractProjectile> projectiles, int timer, int cost, int damage) {
+	public AbstractTower(float x, float y, int range, ArrayList <AbstractProjectile> projectiles, int reloadTime, int cost, int damage) {
 		this.xPos = x*40;
 		this.yPos = y*40;
 		this.range = range;
 		this.projectiles = projectiles;
-		this.timer = timer;
+		this.timer = 0;
+		this.reloadTime = 80;
 		this.cost = cost;
 		this.damage = damage;
+		this.prioritie = Priorities.WEAKEST;
 	}
 	
 	
 	public void tryShoot(List<Wave> waves) {
-		if(timer==20){
-			outerloop:
-			for(Wave w : waves){
-				for(AbstractMonster m: w.getmonstersOnGameBoard()){
-					if(monsterInRange(m)){
-						Projectile p=new Projectile(xPos, yPos, m, damage);
-						projectiles.add(p);
-						break outerloop;
+		if(timer<=0){
+			if(prioritie == Priorities.FIRTS){
+				outerloop:
+				for(Wave w : waves){
+					for(AbstractMonster m: w.getmonstersOnGameBoard()){
+						if(monsterInRange(m)){
+							Projectile p = new Projectile(xPos, yPos, m, damage);
+							projectiles.add(p);
+							timer = reloadTime;
+							break outerloop;
+						}
 					}
+				}			
+			}else if(prioritie == Priorities.WEAKEST){
+				AbstractMonster targetMonster = null;
+				for(Wave w : waves){
+					for(AbstractMonster m: w.getmonstersOnGameBoard()){
+						if(monsterInRange(m)){
+							if(targetMonster == null){
+								targetMonster = m;
+							}else if(m.getLife() < targetMonster.getLife()){
+								targetMonster = m;
+							}
+						}
+					}
+				}	
+				if(targetMonster != null){
+					Projectile p = new Projectile(xPos, yPos, targetMonster, damage);
+					projectiles.add(p);
+					timer = reloadTime;
 				}
-			}			
+			}
+		
 		}		
 		timer--;
-		if(timer<=0){
-			timer=60;
-		}
 		
 	}
 	
@@ -81,6 +104,8 @@ public abstract class AbstractTower {
 		return (int) (this.getCost()*0.5);
 	}
 	
-	public abstract void upgradeTower();
+	public abstract AbstractTower upgradeTower();
+	
+	public abstract int getUpgradeCost();
 
 }
