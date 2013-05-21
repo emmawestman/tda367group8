6a8af3cec8a99f9ef0843c146c4142288b0046a5
EmmaWestman <eewestman@gmail.com>
2013-05-21 12:50:35 +0200

    levels can now have infinity numbers of waves

--- TowerDefence/src/se/chalmers/towerdefence/controller/WaveSplitController.java
+++ TowerDefence/src/se/chalmers/towerdefence/controller/WaveSplitController.java
@@ -4,33 +4,22 @@ import se.chalmers.towerdefence.files.FileHandler;
 
 
 public class WaveSplitController {
-	private String [] allWavesSplited;
+	private String [] allWavesSplit;
 	
 	public WaveSplitController () {
 		FileHandler fh = new FileHandler();
 		String allWaves = fh.getWavesFromFile("level1.txt");
-		int [] splitPoints = findSplitPoints(allWaves);
-		allWavesSplited = separateWaves(allWaves, splitPoints);
+		allWavesSplit = separateWaves(allWaves);
 	}
 
-	public String [] separateWaves (String allWaves, int [] splitPoints) {
-		String [] splitedWaves = allWaves.split(":", splitPoints.length + 1);
-		return splitedWaves;
+	public String [] separateWaves (String allWaves) {
+		String [] splitWaves = allWaves.split(":");
+		return splitWaves;
 	}
 
-	public int [] findSplitPoints (String allWaves) {
-		int [] splitPoints =  new int [3];
-		for ( int i =0; i<allWaves.length(); i++) {
-			if ((allWaves.charAt(i) + "").equals(":")) {
-				int j = 0;
-				splitPoints [j] = i;
-				j++;
-			}
-		}
-		return splitPoints;
-	}
+	
 	
 	public String [] getWaves() {
-		return allWavesSplited;
+		return allWavesSplit;
 	}
 }
